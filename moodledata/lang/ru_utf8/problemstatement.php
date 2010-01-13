334cbe3682b6c9c2e94cf802ae47400041c2d377
anna.sent <anna.sent@6a4981a4-e42f-11de-b184-cf21a9d0b08a>
2010-01-13 19:21:38 +0000

    add lang module

---
+++ moodledata/lang/ru_utf8/problemstatement.php
@@ -0,0 +1,125 @@
+<?PHP // $Id$
+      // problemstatement.php - created with Moodle 1.9.2+ (2008082000)
+$string['selectproblem'] = 'Выберите задачу...';
+$string['lang_0'] = 'C/C++';
+$string['lang_1'] = 'Pascal/Delphi';
+$string['lang_2'] = 'Java';
+$string['lang_3'] = 'Python';
+
+$string['problemdescription'] = 'Постановка задачи';
+$string['restrictions'] = 'Ограничения';
+$string['samples'] = 'Примеры';
+
+$string['problemstatement'] = 'Задача по программированию';
+$string['problemstatements'] = 'Задачи по программированию';
+$string['problemstatementname'] = 'Имя задачи';
+$string['testsdirectory'] = 'Директория с тестами';
+
+$string['programwritten'] = 'Программа написана на ';
+
+$string['submissioncomment'] = 'Тестирование';
+
+$string['unprocessed'] = 'Задача ожидает обработки. Обновите окно через несколько секунд.';
+$string['inprocess'] = 'Задача обрабатывается. Обновите окно через несколько секунд.';
+$string['success'] = 'Тесты успешно пройдены!!!! :-)';
+$string['unsuccess'] = 'Тесты провалены!!!! :-(';
+$string['compilationerror'] = 'Ошибка компиляции!!! :-|';
+$string['internalerror'] = 'Ошибка на сервере. Сообщите администратору';
+$string['timeout'] = 'Ваша программа превысила лимит времени.';
+$string['memoryout'] = 'Ваша программа превысила лимит памяти.';
+$string['runtimeerror'] = 'Ошибка времени выполнения.';
+
+$string['allowdeleting'] = 'Разрешить удаление';
+$string['allowmaxfiles'] = 'Максимальное число загружаемых файлов';
+$string['allownotes'] = 'Разрешить комментарии';
+$string['allowresubmit'] = 'Несколько попыток';
+$string['assignmentdetails'] = 'Детали задания';
+$string['assignmentmail'] = '$a->teacher написал отзыв на Ваше задание \'$a->assignment\'.
+
+Вы можете посмотреть его по ссылке:
+$a->url';
+$string['assignmentmailhtml'] = '$a->teacher написал отзыв на Ваше задание \'<i>$a->assignment</i>\'<br /><br />
+Вы можете посмотреть его  <a href=\"$a->url\">на сайте</a>.';
+$string['assignmentname'] = 'Название задания';
+$string['assignmenttype'] = 'Тип задания';
+$string['availabledate'] = 'Доступно с';
+$string['comment'] = 'Отзыв';
+$string['commentinline'] = 'Включить в отзыв текст ответа студента';
+$string['configmaxbytes'] = 'Установленный по умолчанию максимальный размер для всех заданий на сайте (соотносится с установленным ограничением по курсу и с другими локальными установками)';
+$string['description'] = 'Описание';
+$string['draft'] = 'Черновик';
+$string['duedate'] = 'Последний срок сдачи';
+$string['duedateno'] = 'Срок сдачи не ограничен';
+$string['early'] = '$a рано';
+$string['editmysubmission'] = 'Редактировать моё решение';//
+$string['emailteachermail'] = '$a->username отправил(а) новый ответ на задание \'$a->assignment\'
+Ответы на это задание доступны по адресу:
+$a->url';
+$string['emailteachermailhtml'] = '$a->username отправил(а) новый ответ на задание <i>$a->assignment</i><br /><br />
+Ответы на это задание <a href=\"$a->url\">расположены на сайте</a>.';
+//$string['emailteachers'] = 'Отправлять уведомления учителям';
+$string['emptysubmission'] = 'Вы ещё ничего не отправляли.';
+$string['existingfiledeleted'] = 'Существующий файл был удален: $a';
+$string['failedupdatefeedback'] = 'Не удалось обновить отзыв пользователю $a';
+$string['feedback'] = 'Отзыв';
+$string['feedbackfromteacher'] = 'Отзыв от $a';
+$string['feedbackupdated'] = 'Ответ изменен для $a';
+$string['guestnosubmit'] = 'Извините, гость не может отправлять ответ на задание. Вам необходимо <a href=\"../../login/\" title=\"Перейти на страницу входа в систему\">войти под своей учетной записью</a> (или <a href=\"../../login/signup.php\" title=\"Перейти на страницу создания новой учетной записи\">создать новую</a>, если у вас ее нет), чтобы вы могли отправить ответ';//
+$string['guestnoupload'] = 'Извините, гости не могут отвечать на задания';
+$string['helpoffline'] = '<p>Этот режим полезен, когда задание должно быть выполнено вне Moodle.</p>
+<p>Например, где-либо в сети или лицом к лицу с учителем. Студенты могут читать пояснение к заданию, но не могут прислать ни текст, ни файл в ответ.</p>
+<p>Учитель же, как и в других видах, может написать свой отзыв и поставить оценку.</p>';
+$string['helponline'] = '<p>Этот режим предполагает возможность студента дать ответ онлайн. Студент может редактировать текст ответа, используя обычные средства редактирования (как в форуме, при отправке сообщения). </p>
+<p>Учитель может написать отзыв на ответ ученика и поставить оценку. Свой отзыв учитель может писать, взяв за основу ответ, присланный студентом, т.е вносить исправления, комментировать и т.п. При этом учитель не должен забывать выделять свои комментарии хотя бы цветом.</p>
+<p>Для тех, кто знаком с предыдущими версиями moodle, скажем, что этот режим работы задания похож на использование рабочей тетради</p>';
+$string['helpuploadsingle'] = '<p>Этот режим предполагает возможность ученика отправить в качестве ответа произвольный файл. Например, картинку, документ Word, архив, или что угодно другое.</p>';
+$string['hideintro'] = 'Скрыть описание пока задание не станет доступным';
+$string['language'] = 'Язык программирования';
+$string['late'] = 'Срок выполнения закончился $a назад';
+$string['maximumgrade'] = 'Максимальная оценка';
+$string['maximumsize'] = 'Максимальный размер';
+$string['modulename'] = 'Задача по программированию';//
+$string['modulenameplural'] = 'Задачи по программированию';//
+$string['newsubmissions'] = 'Предложить ответ';
+$string['noassignments'] = 'Ответов еще нет';
+$string['noattempts'] = 'Ни одной попытки не было выполнено для размещения решения задачи'; //
+$string['nofilesyet'] = 'Ни одного файла еще не отправлено';
+$string['notes'] = 'Комментарии';
+$string['notesempty'] = 'Нет записей';
+$string['notgradedyet'] = 'Пока не оценено';
+$string['notsubmittedyet'] = 'Решение пока не отправлено';
+$string['onceassignmentsent'] = 'Если Вы отправите задание для получения оценки, Вы больше не сможете удалять или прикреплять файлы. Хотите продолжить?';
+$string['overwritewarning'] = 'Предупреждение: Ваш новый ответ заменит предыдущий';
+$string['pagesize'] = 'Количество ответов на странице';
+$string['preventlate'] = 'Запретить отправку ответа после истечения срока выполнения';
+$string['problemstatementname'] = 'Задача по программированию';
+$string['quickgrade'] = 'Перейти в режим быстрой оценки работ';
+$string['responsefiles'] = 'Ответные файлы';
+$string['saveallfeedback'] = 'Сохранить все исправления';
+$string['sendformarking'] = 'Отправить для получения оценки';
+$string['submission'] = 'Код решения задачи';//
+$string['submissiondraft'] = 'Черновик ответов';
+$string['submissionfeedback'] = 'Ответ исправлен';
+$string['submissions'] = 'Посылки';//
+$string['submissionsaved'] = 'Ваши изменения сохранены';//
+$string['submitassignment'] = 'Отправьте Ваш ответ, используя форму';
+$string['submitedformarking'] = 'Задание отправлено для получения оценки и не может быть обновлено';
+$string['submitformarking'] = 'Окончательная отправка для получения оценки';
+$string['submitted'] = 'Ответы и отзывы';
+$string['typeoffline'] = 'Ответ - вне сайта';
+$string['typeonline'] = 'Ответ - в виде текста';
+$string['typeupload'] = 'Ответ - в виде нескольких файлов';
+$string['typeuploadsingle'] = 'Ответ - в виде файла';
+$string['unfinalize'] = 'Вернуть на доработку';
+$string['uploadbadname'] = 'Этот файл содержит недопустимые символы и не может быть закачан';
+$string['uploadedfiles'] = 'Закачанные файлы';
+$string['uploaderror'] = 'При отправке файла на сервер произошла ошибка';
+$string['uploadfailnoupdate'] = 'Файл был закачан, но не обновить ваш предыдущий ответ не удалось!';
+$string['uploadfiletoobig'] = 'Файл слишком большой (ограничение в $a байт)';
+$string['uploadnofilefound'] = 'Файл не был найден. Вы уверены, что выбрали его для закачки?';
+$string['uploadnotregistered'] = '\'$a\' был закачан, но ответ не зарегистрирован';
+$string['uploadsuccess'] = 'Закачка файла \'$a\' завершена';
+$string['viewfeedback'] = 'Посмотреть оценки задания и отзывы';
+$string['viewsubmissions'] = 'Решений задачи - $a (посмотреть)'; //
+$string['yoursubmission'] = 'Ваш ответ';
+?>
